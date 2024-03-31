d857a88578136bd12380fc63710b4ac22186c57c
Collins Gichuki <gichukimuchiri80@gmail.com>
2024-03-31 16:50:41 +0300

    to-do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let todoList = document.querySelector("form");
+  todoList.addEventListener("submit", function (e) {
+    e.preventDefault();
+    addTask(e.target.new_task_description.value);
+    todoList.reset();
+  });
 });
+
+function addTask(todoTasks) {
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", dltTask);
+  btn.textContent = "X";
+  li.textContent = `${todoTasks} `;
+  li.appendChild(btn);
+  document.querySelector("#tasks").appendChild(li);
+}
+
+function dltTask(e) {
+  e.target.parentNode.remove();
+}
